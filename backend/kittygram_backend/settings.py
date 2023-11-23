e53eff1e6704ad415ddd4f432931730598ae5fec
Данила Полунин <d.polunin2001@gmail.com>
2023-11-23 15:52:25 +0300

    Add env.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django_secret_key')
 
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['158.160.71.160', '127.0.0.1', 'localhost', 'https://wiz410.freedynamicdns.net/']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -82,7 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv('DJANGO_TIME_ZONE', 'UTC')
 
 USE_I18N = True
 
